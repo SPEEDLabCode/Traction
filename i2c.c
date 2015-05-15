5907f131069e3b2bbda35f3453509ce49468beac
Will Anthony <willanth@gmail.com>
2015-05-15 10:25:07 +0930

    Removal of faulty and obsolete libraries and addition of headers
    Addition of header information to I2C libraries and removal of the
    obsolete gps libraries, replaced with the device specific libraries

--- i2c.c
+++ i2c.c
@@ -1,5 +1,39 @@
 
-//#include "P24FJ256GB106.h" /* Register and bit declarations */
+/*
+ * File:   i2c.c.c
+ * Author:
+ * E. Freund <eric@affinityengineering.com.au
+ * W. Anthony <will@affinityengineering.com.au>
+ * Description: This is the header file for the built-in I2C transciever.
+ *
+ * Language: C (CCI)
+ * Coding Standard: NASA JPL DOCID D-60411
+ * Style: NASA SEL-94-003
+ * License: GNU GPL version 3.0
+ * Version: 1.0
+ *
+ * Dependancies:
+ * SIM800.h
+ * Compiler: Microchip XC16
+ * Compiler Revision: 1.24
+ *
+ * Created on 16 April 2015, 11:43 PM
+ * Copyright (C) 2015  Affinity Engineering pty ltd
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <xc.h>
 #include "i2c.h"
 
